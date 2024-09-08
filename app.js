9a2e443c12c76b3e09470d4daf3b77ff5f20e2a0
Arhyel24 <arhyelphilip024@gmail.com>
2024-09-08 18:49:41 +0100

    trying netlify deployment, ps i need to get credit card

--- app.js
+++ app.js
@@ -98,8 +98,8 @@ app.use("*", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
-export default app;
+// export default app;
